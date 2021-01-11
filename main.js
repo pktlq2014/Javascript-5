34e46bc0fc3903508a15d4f7b92ec480ca9d3574
unknown <nguyenvanty10987654321000@gmail.com>
2021-01-11 18:54:06 +0700

    update 2

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // chèn vào giữa dùng: splice (còn có thể dùng để xóa), cuối dùng push, đầu dùng shjft
-// filter cũng có thể dùng để xóa: VD xóa 3 thì lọc < 3 && > 3
+
+// filter (lặp qua và lấy all object nào thỏa điều kiện, còn find thì chỉ lấy duy nhất 1 thằng 
+// và thằng đó là thằng phát hiện, tìm thấy đầu tiên và thỏa điều kiện đầu tiên) 
+//cũng có thể dùng để xóa: VD xóa 3 thì lọc < 3 && > 3
+
 // tìm vị trí của 1 phần tử mong muốn thì dùng forEach()
 var lang = ["vietnam", "chinese", "japan"];
 console.log("test 1: ");
@@ -37,7 +41,7 @@ arr3.push(70);
 arr3.push(80);
 console.log("test 11: " + arr3);
 // lấy phần tử bắt đầu từ vị trí 1 và < 3
-var item1 = arr3.slice(1,3);
+var item1 = arr3.slice(1, 3);
 console.log("test 12: " + arr3);
 console.log("test 13: " + item1);
 var arr4 = [13, 45, 15, 55, 96];
@@ -49,28 +53,28 @@ var arr4 = [13, 45, 15, 55, 96];
 //     }
 // }
 // hoặc
-var arr5 = arr4.filter(function(value){
+var arr5 = arr4.filter(function (value) {
     return value > 40 && value < 60;
 });
 console.log("test 15: " + arr5);
 var arr6 = [
     {
-        id : 1,
-        name : "thiennv",
-        school : "fpoly"
+        id: 1,
+        name: "thiennv",
+        school: "fpoly"
     },
     {
-        id : 2,
-        name : "dungnv",
-        school : "fpoly"
+        id: 2,
+        name: "dungnv",
+        school: "fpoly"
     },
     {
-        id : 3,
-        name : "longnv",
-        school : "zpoly"
+        id: 3,
+        name: "longnv",
+        school: "zpoly"
     }
 ]
-var arr7 = arr6.filter(function(item){
+var arr7 = arr6.filter(function (item) {
     return item.school === "fpoly";
 });
 console.log("test 16: ");
@@ -88,13 +92,13 @@ console.log("test 19: " + arr8);
 // xóa phần tử 'fu'
 var arr9 = ['fpoly', 'fu', 'fschool', 'fu', 'thschool', 'fu'];
 // dùng filter
-var arr10 = arr9.filter(function(value) {
+var arr10 = arr9.filter(function (value) {
     return value !== 'fu';
 });
 console.log("test 20: " + arr10);
 // dùng splice
-for(var i = 0; i < arr9.length; i++) {
-    if(arr9[i] === 'fu') {
+for (var i = 0; i < arr9.length; i++) {
+    if (arr9[i] === 'fu') {
         arr9.splice(i, 1);
     }
 }
@@ -102,14 +106,14 @@ console.log("test 21: " + arr9);
 // tìm các vị trí xuất hiện 'fu'
 var arr11 = ['fpoly', 'fu', 'fschool', 'fu', 'thschool', 'fu'];
 var arr12 = [];
-arr11.forEach(function(value, index) {
-    if(value === 'fu') {
+arr11.forEach(function (value, index) {
+    if (value === 'fu') {
         arr12.push(index);
     }
 });
 console.log("test 22: " + arr12);
 // cách khác, áp dụng thư viện nhưng chỉ tìm được duy nhất 1 vị trí phần tử cần tìm mà thôi
-var arr13 = _.findIndex(arr11, function(value) {
+var arr13 = _.findIndex(arr11, function (value) {
     return value === 'fu';
 });
 console.log("test 28: " + arr13);
@@ -123,8 +127,8 @@ console.log("test 36: " + arr15);
 // thay thế toàn bộ chuỗi chỉ định xuất hiện trong chuỗi A thành chuỗi khác
 // kết hợp indexOf + replace
 var arr16 = "helloweb pt14313-web";
-while(arr16.indexOf('web') !== -1) {
-    arr16 = arr16.replace('web', 'vanty');   
+while (arr16.indexOf('web') !== -1) {
+    arr16 = arr16.replace('web', 'vanty');
 }
 console.log("test 37: " + arr16);
 var arr17 = "ABCBA";
@@ -134,7 +138,7 @@ var arr18 = arr17.split("");
 var arr19 = arr18.reverse();
 // convert từ mảng -> chuỗi lại để so sánh dùng join("")
 arr19 = arr19.join("");
-if(arr19 === arr17) {
+if (arr19 === arr17) {
     console.log("test 38 ok - " + arr19 + " - " + arr17);
 }
 else {
@@ -143,16 +147,195 @@ else {
 var arr20 = "nguy";
 var arr21 = 3;
 var arr22 = "";
-for(var i = 0; i < arr20.length; i++) {
-    if(i < arr21) {
+for (var i = 0; i < arr20.length; i++) {
+    if (i < arr21) {
         arr22 += arr20[i];
     }
-    else{
+    else {
         arr22 += ".";
     }
 }
 arr22 += ".";
 console.log("test 39: " + arr22);
+
+
+
+
+
+confirm("test 40");
+prompt("test 41");
+var arr23 = [
+    {
+        id: 1,
+        name: "A",
+        coin: 100
+    },
+    {
+        id: 2,
+        name: "B",
+        coin: 100
+    },
+    {
+        id: 3,
+        name: "C",
+        coin: 100
+    },
+];
+// forEach giúp duyệt qua all object, vừa trả về value vừa trả về index
+arr23.forEach(function (value, index) {
+    console.log("test 42: " + index + " - ");
+    console.log(value);
+});
+// every cũng giống với forEach nhưng nó hỗ trợ thêm kiểm tra điều kiện
+// kiểm tra hết toàn bộ all object, kiểm tra 1 thuộc tính bất kỳ mà all object phải thỏa
+// nếu all object đều thỏa thì trả về true, ngược lại có 1 trong all object không thỏa
+// thì trả về false
+var arr24 = arr23.every(function (value, index) {
+    return value.coin === 100;
+});
+console.log("test 43: " + arr24);
+var arr26 = [
+    {
+        id: 1,
+        name: "A",
+        coin: 0
+    },
+    {
+        id: 2,
+        name: "C",
+        coin: 0
+    },
+    {
+        id: 3,
+        name: "C",
+        coin: 100
+    },
+];
+// some cũng giống với forEach và every duyệt qua all object, nhưng nó hỗ trợ thêm kiểm tra điều kiện
+// kiểm tra hết toàn bộ all object, kiểm tra 1 thuộc tính bất kỳ mà chỉ cần 1 object thỏa trong nhiều object
+// thì trả về true ngược lại
+// thì trả về false
+// khác với thằng every thì chỉ cần 1 thằng thỏa thôi cũng trả về true nữa
+var arr25 = arr26.some(function (value, index) {
+    return value.coin === 100;
+});
+console.log("test 44: " + arr25);
+// find duyệt qua all object, giúp tìm 1 thuộc tính bất kỳ nằm trong all object
+// thuộc tính nào của object nào thỏa điều kiện thì trả về toàn bộ object chứa thuộc tính đó
+// điều kiện thuộc tính của object nào tìm thấy trước thì trả về toàn bộ object chứa thuộc tính đó trước
+// những thằng phía sau dù thỏa điều kiện cũng không lấy, chỉ lấy ra và trả về 1 object duy nhất
+// là object thỏa điều kiện được tìm thấy đầu tiên
+var arr27 = arr26.find(function (value, index) {
+    return value.name === "C";
+});
+console.log("test 45: ");
+console.log(arr27);
+// filter cũng giống như find duyệt qua all object, giúp tìm 1 thuộc tính bất kỳ nằm trong all object
+// thuộc tính nào của object nào thỏa điều kiện thì trả về toàn bộ all những object chứa thuộc tính đó
+// ngược lại với find là nó lọc và lấy all object chứa điều kiện thỏa, chứ không lấy duy nhất 1 object
+// thỏa điều kiện or phát hiện đầu tiên mà thôi.
+var arr28 = arr26.filter(function (value, index) {
+    return value.name === "C";
+});
+console.log("test 46: ");
+console.log(arr28);
+// map giúp duyệt qua all object, ngoài ra còn giúp chỉnh sửa dữ liệu của thuộc tính trong mỗi object
+// hoặc có thể thêm thuộc tính mới vào object
+// ngoài ra map còn giúp lấy ra 1 or nhiều thuộc tính bất kỳ nằm trong all object để lưu vào 1 mảng mới
+var arr29 = arr26.map(function (value, index) {
+    return {
+        id: value.id,
+        // chỉnh sửa dữ liệu trong thuộc tính name
+        name: `Khóa Học ${value.name}`,
+        coin: value.coin,
+        coinText: 'Very Good'
+    };
+});
+console.log("test 47: ");
+console.log(arr29);
+// ngoài ra map còn giúp lấy ra giá trị 1 thuộc tính bất kỳ nằm trong all object để lưu vào 1 mảng mới
+var arr30 = arr26.map(function (value, index) {
+    // return {
+    //     name : value.name,
+    //     coin : value.coin
+    //  }
+
+    //return value.name;
+
+    return `
+        <div>
+            <h2>${value.name}</h2>
+        </div>
+    `;
+});
+console.log("test 48: ");
+console.log(arr30.join(""));
+// reduce giúp duyệt qua all object, lấy giá trị của 1 thuộc tính bất kỳ nằm trong all object
+// ra tính toán và trả về giá trị tổng cộng sau khi duyệt qua xong all object
+// tính toán thì dùng thằng này
+// 0 là giá trị mặc định ban đầu để tính toán, thay vì để biến count = 0 thì hàm này hỗ trợ sẵn
+// 1 biến trong hàm, giá trị mặc định này có thể là chuỗi, số float, mảng cũng được
+
+var arr31 = arr26.reduce(total, 0)
+// tempInitial là giá trị ban đầu khởi tạo = 0
+function total(tempInitial, value) {
+    return tempInitial + value.coin;
+};
+console.log("test 49: ");
+console.log(arr31);
+// gôm 2 hàm trên lại ngắn gọn và khó hiểu hơn
+// tempInitial = 0;
+var arr32 = arr26.reduce(function (tempInitial, value) {
+    return tempInitial + value.coin;
+}, 0);
+console.log("test 50: ");
+console.log(arr32);
+// VD khác về reduce, làm phẳng mảng gọi là Flat
+var arr33 = [1, 2, [3, 4], 5, 6, [7, 8, 9]];
+var arr34 = arr33.reduce(function (tempInitial, value) {
+    return tempInitial.concat(value);
+}, []);
+console.log("test 51: " + arr34);
+// VD khác về reduce, làm phẳng mảng gọi là Flat kết hợp object
+var arr35 = [
+    {
+        topic: "Front-end",
+        courses: [
+            {
+                id: 1,
+                title: "HTML, CSS"
+            },
+            {
+                id: 2,
+                title: "ReactJS"
+            }
+        ]
+    },
+    {
+        topic: "Back-end",
+        courses: [
+            {
+                id: 1,
+                title: "JAVA"
+            },
+            {
+                id: 2,
+                title: "PHP"
+            }
+        ]
+    },
+];
+var arr36 = arr35.reduce(function (tempInitial, value) {
+    return tempInitial.concat(value.courses);
+}, []);
+console.log("test 52: ");
+console.log(arr36);
+var box1 = document.querySelector(".box1");
+var box2 = document.querySelector(".box2");
+box1.innerText = "<h1>AAA</h1>";
+box2.innerHTML = "<h1>AAA</h1>";
+
+
 
 
 //                                      DATE 
@@ -183,4 +366,4 @@ var shuffArr = _.shuffle([1, 2, 3, 4]);
 console.log("test 26: " + shuffArr);
 // tìm ra các số khác trong mảng A từ mảng B 
 // -> số nào tồn tại trong mạng A mà không tồn tại trong mảng B sẽ dc return về và lấy giá trị đó
-console.log("test 27: " + _.difference([1, 2, 3, 4, 5], [2, 4 ,5 ,6]));
+console.log("test 27: " + _.difference([1, 2, 3, 4, 5], [2, 4, 5, 6]));
